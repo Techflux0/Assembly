2e0971bebc7b40907d4264154cf93a4e9c47ce73
綾小路 <134218160+Techflux0@users.noreply.github.com>
2025-07-28 23:47:39 +0300

    ...

--- main.asm
+++ main.asm
@@ -1,15 +1,18 @@
-
-
 section .data
-    msg db "Hello, World!", 10, 0         ; message + newline + null terminator
+    fmt db "Result is: %d", 10, 0
+    num1 dq 20
+    num2 dq 22
 
 section .text
     extern printf
     global main
 
 main:
-    sub rsp, 40       
-    lea rcx, [rel msg]  
+    sub rsp, 40
+    mov rax, [rel num1]
+    add rax, [rel num2]
+    mov rcx, fmt
+    mov rdx, rax
     call printf
     add rsp, 40
     ret
