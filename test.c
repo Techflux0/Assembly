2b1774415521f69c79d2246c25d47d62545e9540
綾小路 <134218160+Techflux0@users.noreply.github.com>
2025-07-29 16:06:05 +0300

    ...

--- test.c
+++ test.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
-int main(){
-    printf("Hello world");
+char msg[] = "Hello from assembly!\n";
+int len = sizeof("Hello from assembly!\n") - 1;
 
+int main() {
+    __asm__ volatile (
+        "mov $1, %%rax\n\t"        
+        "mov $1, %%rdi\n\t"        
+        "mov %[m], %%rsi\n\t"      
+        "mov %[l], %%rdx\n\t"      
+        "syscall\n\t"
+        :
+        : [m] "r"(msg), [l] "r"((long)len)
+        : "rax", "rdi", "rsi", "rdx"
+    );
     return 0;
 }
